3933a3dbaaa48a982e4cc621dd1415f03f7aae01
Adam Walter <adam.iverson.walter@gmail.com>
2017-10-25 17:00:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,19 @@ def long_planeteer_calls(calls)
   calls.any? { |call| call.length > 4  }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(string)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  if string.include?(cheese_types[0])
+    puts cheese_types[0]
+  elsif string.include?cheese_types[1]
+    puts cheese_types[1]
+  elsif string.include?(cheese_types[2])
+    puts cheese_types[2]
+  else
+    puts nil
+  end
+
 end
+
+
